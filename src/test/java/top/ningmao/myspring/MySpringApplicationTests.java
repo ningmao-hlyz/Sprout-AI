9ccbd9946f03aec9eca65ba32f5c36a4c836c148
ningmao-hlyz <2997825003@qq.com>
2025-04-29 13:43:38 +0800

    Initial commit

---
+++ src/test/java/top/ningmao/myspring/MySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ningmao.myspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
